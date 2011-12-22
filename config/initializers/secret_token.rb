cc6a82c60b88ac83404dc4a77766a50536a67371
John Dowd <dowdjj@gmail.com>
2011-12-22 09:36:00 -0500

    This is my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flashlight::Application.config.secret_token = 'fcb42d99977d9553c45226981cc055577bc2282959aef830b650d8f2cbf29d6c2f41f0f6b5ce4be51cb8f005d27a57d9fb5f83bf08d409c5262681650f392c2f'
